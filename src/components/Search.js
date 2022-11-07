ca40d620ae318d82e0cec00fe89bac084130add1
Dylan Thomas <thomasdylan76@gmail.com>
2022-11-06 20:02:19 -0700

    Added Search Component

---
+++ src/components/Search.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Search = () => {
+    const [term, setTerm] = useState('');
+
+    useEffect(() => {
+        const search = async () => {
+            await axios.get('');
+        };
+
+        search();
+    }, [term]);
+    return (
+        <div className='ui form'>
+            <div className='field'>
+                <label>Enter Search Term</label>
+                <input
+                    className='input'
+                    value={term}
+                    onChange={e => setTerm(e.target.value)}
+                />
+            </div>
+        </div>
+    )
+};
+
+export default Search;
+
+
